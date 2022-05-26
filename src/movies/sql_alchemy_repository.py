d8c2b85bab8f710847a0658acec6d4d9cd5a547c
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-05-25 19:28:04 -0500

    Fix config

--- src/movies/sql_alchemy_repository.py
+++ src/movies/sql_alchemy_repository.py
@@ -48,9 +48,10 @@ def create_scoped_session(
 
 def get_postgres_uri():
     host = os.environ.get("DB_HOST", "postgres")
-    port = 5432
+    port = os.environ.get("DB_PORT", 5432)
     password = os.environ.get("DB_PASS", "abc123")
-    user, db_name = "movies", "movies"
+    user = os.environ.get("DB_USER", "movies")
+    db_name = os.environ.get("DB_NAME", "movies")
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 class SqlAlchemyRepository(BaseRepository):
